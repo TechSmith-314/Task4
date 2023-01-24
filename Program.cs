be97e21fc2d2111904fabe47ef1582a6b98e7dc8
eseni <eseni@DESKTOP-TM01D22>
2023-01-24 18:42:58 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userZodiacSign;
+            string userAge;
+            string userJob;
+
+            Console.WriteLine("Как Вас зовут?");
+            userName = Console.ReadLine();
+            Console.WriteLine("Какой Ваш знак зодиака?");
+            userZodiacSign = Console.ReadLine();
+            Console.WriteLine("Где Вы работаете?");
+            userJob = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет?");
+            userAge = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {userName}, Вам {userAge} год, Вы {userZodiacSign} и работаете на {userJob}.");
+
+
+        }
+    }
+}
